343db055ec64f41e69af8ca2ebc5b9f5ab8f590c
AmirrezaHeidari <amirheidari020@gmail.com>
2024-06-01 11:26:25 +0430

    change carousel

--- src/components/shared/CarouselPosterList.tsx
+++ src/components/shared/CarouselPosterList.tsx
@@ -17,8 +17,10 @@ import {
   InfiniteData,
   InfiniteQueryObserverResult,
 } from "@tanstack/react-query";
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import { Link } from "react-router-dom";
+import Slider from "react-slick";
+import useSize from "@/hooks/useSize";
 
 type props = {
   title: string;
@@ -31,7 +33,7 @@ type props = {
     options?: FetchNextPageOptions | undefined
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
   ) => Promise<InfiniteQueryObserverResult<InfiniteData<any, unknown>, Error>>;
-  mediaType?:string
+  mediaType?: string;
 };
 
 const CarouselPosterList = ({
@@ -40,20 +42,55 @@ const CarouselPosterList = ({
   isLoading,
   hasNextPage,
   fetchNextPage,
-  mediaType
+  mediaType,
 }: props) => {
   const { ref, inView } = useInView();
+
+  const windowSize = useSize();
+
+  const settings = {
+    infinite: false,
+    slidesToShow: Math.ceil(windowSize[0] / 285),
+    slidesToScroll: Math.ceil(windowSize[0] / 285),
+    nextArrow: <div className="w-5 h-20 bg-white"></div>,
+    prevArrow: (
+      <div
+        className="w-5 h-20 bg-red-200"
+        style={{ display: "block", background: "red" }}
+      ></div>
+    ),
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          infinite: false,
+          slidesToShow: Math.ceil(windowSize[0] / 285),
+          slidesToScroll:Math.ceil(windowSize[0] / 285),
+          nextArrow: <div className="w-5 h-20 bg-white"></div>,
+          prevArrow: (
+            <div
+              className="w-5 h-20 bg-red-200"
+              style={{ display: "block", background: "red" }}
+            ></div>
+          ),
+        },
+      },
+    ],
+  };
 
   useEffect(() => {
     if (inView && fetchNextPage && items) {
       fetchNextPage();
     }
+    console.log("eybaba");
   }, [inView]);
 
   return (
     <div className=" flex flex-col gap-5 mt-5">
       <header>
-        <h2 className=" text-white font-semibold text-xl md:text-3xl px-10">{title}</h2>
+        <h2 className=" text-white font-semibold text-xl md:text-3xl px-10">
+          {title}
+        </h2>
       </header>
       <main className="sm:px-1 px-0  pb-10 relative ">
         {isLoading ? (
@@ -66,122 +103,126 @@ const CarouselPosterList = ({
           </div>
         ) : (
           <>
-            <Carousel className="w-full  cursor-pointer  ">
-              <CarouselContent className=" gap-2  relative ">
-                {items &&
-                  (items.results !== undefined ? items.results : items).map(
-                    (card: Media) => (
-                      <Link to={mediaType? `/${mediaType}/${card?.id}` : card.media_type ==='movie'?  `/movie/${card?.id}`: `/tv/${card?.id}`}>
-                        <CarouselItem
-                          onClick={() => console.log(items)}
-                          key={card?.id}
-                          className="max-w-fit p-0 text-white flex flex-col gap-5 "
-                        >
-                          <Card className=" border-4 border-black/50 ">
-                            <CardContent className=" p-0 relative ">
-                              <div className="relative">
+            <Slider className="mx-10  cursor-pointer  " {...settings}>
+              {items &&
+                (items.results !== undefined ? items.results : items).map(
+                  (card: Media) => (
+                    <Link
+                      to={
+                        mediaType
+                          ? `/${mediaType}/${card?.id}`
+                          : card.media_type === "movie"
+                          ? `/movie/${card?.id}`
+                          : `/tv/${card?.id}`
+                      }
+                    >
+                      <div
+                        onClick={() => console.log(items)}
+                        key={card?.id}
+                        className="max-w-fit p-0 text-white flex flex-col gap-5 "
+                      >
+                        <Card className=" border-4 border-black/50 ">
+                          <CardContent className=" p-0 relative ">
+                            <div className="relative">
+                              <img
+                                src={`https://image.tmdb.org/t/p/w500${card?.poster_path}`}
+                                className="min-w-[100px] h-[150px] md:min-w-[200px] md:h-[300px] rounded-md  hover:bg-gradient-to-t hover:from-black"
+                                alt=""
+                              />
+                              <div className="h-full w-full absolute bottom-0 opacity-0 hover:opacity-100 hover:bg-gradient-to-t hover:from-black/70  flex">
                                 <img
-                                  src={`https://image.tmdb.org/t/p/w500${card?.poster_path}`}
-                                  className="min-w-[100px] h-[150px] md:min-w-[200px] md:h-[300px] rounded-md  hover:bg-gradient-to-t hover:from-black"
-                                  alt=""
-                                  
+                                  className="m-auto w-10 h-10 md:w-16  md:h-16 opacity-75 bg-black rounded-full p-2"
+                                  src="/assets/icons/right-arrow.svg"
                                 />
-                                <div className="h-full w-full absolute bottom-0 opacity-0 hover:opacity-100 hover:bg-gradient-to-t hover:from-black/70  flex">
-                                  <img className="m-auto w-10 h-10 md:w-16  md:h-16 opacity-75 bg-black rounded-full p-2" src="/assets/icons/right-arrow.svg" />
-                                </div>
                               </div>
-                              <div>
-                                <div className="p-2 w-fit h-fit absolute top-0 left-0 md:top-2 md:left-2 rounded-full">
+                            </div>
+                            <div>
+                              <div className="p-2 w-fit h-fit absolute top-0 left-0 md:top-2 md:left-2 rounded-full">
                                 <img
                                   src="/assets/icons/heart.svg"
                                   alt=""
                                   className=" max-md:w-6 bg-black/70 p-1 rounded-full"
                                 />
-                                </div>
-                                <div className="p-2 w-fit h-fit absolute top-0 right-0 md:top-2 md:right-2 rounded-full">
+                              </div>
+                              <div className="p-2 w-fit h-fit absolute top-0 right-0 md:top-2 md:right-2 rounded-full">
                                 <img
                                   src="/assets/icons/save.svg"
                                   alt=""
                                   className=" brightness-200  max-md:w-6 bg-black/70 max-md:p-1 md:p-[6px] rounded-full "
                                 />
-                                </div>
                               </div>
+                            </div>
 
-                              <Skeleton className="h-full w-full bg-neutral-600 absolute top-0 left-0 -z-10">
-                                <img
-                                  src="/assets/icons/Spinner-2.gif"
-                                  alt=""
-                                  className="absolute w-10 h-10 top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2"
-                                />
-                              </Skeleton>
+                            <Skeleton className="h-full w-full bg-neutral-600 absolute top-0 left-0 -z-10">
+                              <img
+                                src="/assets/icons/Spinner-2.gif"
+                                alt=""
+                                className="absolute w-10 h-10 top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2"
+                              />
+                            </Skeleton>
 
-                              <div className="absolute -bottom-5 left-2 md:w-16 md:h-16 w-10 h-10">
-                                <CircularProgressbar
-                                  value={card?.vote_average}
-                                  styles={buildStyles({
-                                    rotation: 0,
+                            <div className="absolute -bottom-5 left-2 md:w-16 md:h-16 w-10 h-10">
+                              <CircularProgressbar
+                                value={card?.vote_average}
+                                styles={buildStyles({
+                                  rotation: 0,
 
-                                    strokeLinecap: "butt",
+                                  strokeLinecap: "butt",
 
-                                    textSize: "20px",
+                                  textSize: "20px",
 
-                                    pathTransitionDuration: 0.5,
+                                  pathTransitionDuration: 0.5,
 
-                                    pathColor: "#DC2626",
-                                    textColor: "#fff",
-                                    trailColor: "#fff",
-                                    backgroundColor: "#111111",
-                                  })}
-                                  strokeWidth={8}
-                                  maxValue={10}
-                                  text={`${
-                                    parseFloat(card?.vote_average?.toFixed(1)) *
-                                    10
-                                  }`}
-                                  background={true}
-                                  backgroundPadding={8}
-                                />
-                              </div>
-                            </CardContent>
-                          </Card>
+                                  pathColor: "#DC2626",
+                                  textColor: "#fff",
+                                  trailColor: "#fff",
+                                  backgroundColor: "#111111",
+                                })}
+                                strokeWidth={8}
+                                maxValue={10}
+                                text={`${
+                                  parseFloat(card?.vote_average?.toFixed(1)) *
+                                  10
+                                }`}
+                                background={true}
+                                backgroundPadding={8}
+                              />
+                            </div>
+                          </CardContent>
+                        </Card>
 
-                          <div className="flex flex-col text-center">
-                            <p className="max-w-[100px] md:max-w-[200px] max-md:text-xs line-clamp-2 text-ellipsis">
-                              {card?.title
-                                ? card?.title
-                                : card?.original_title
-                                ? card?.original_title
-                                : card?.name}
-                            </p>
-                            <p className="opacity-60 text-sm">
-                              {card?.release_date?.slice(0, 4)}
-                            </p>
-                          </div>
-                        </CarouselItem>
-                      </Link>
-                    )
-                  )}
-
-                {hasNextPage && items && (
-                  <div className=" " ref={ref}>
-                    <Skeleton className="h-[220px] w-[110px] md:h-[380px] md:w-[200px] flex flex-col-reverse gap-2 items-center  p-2  bg-neutral-800 relative">
-                      <Skeleton className="  w-10 h-4  bg-neutral-600" />
-                      <Skeleton className="   w-full h-4   bg-neutral-600" />
-                      <Skeleton className=" w-8 h-8   md:w-14 md:h-14 rounded-full -translate-x-[80%] bg-neutral-600" />
-                      <img
-                        src="/assets/icons/Spinner-2.gif"
-                        alt=""
-                        className="absolute w-10 h-10 top-1/3"
-                      />
-                    </Skeleton>
-                  </div>
+                        <div className="flex flex-col text-center">
+                          <p className="max-w-[100px] md:max-w-[200px] max-md:text-xs line-clamp-2 text-ellipsis">
+                            {card?.title
+                              ? card?.title
+                              : card?.original_title
+                              ? card?.original_title
+                              : card?.name}
+                          </p>
+                          <p className="opacity-60 text-sm">
+                            {card?.release_date?.slice(0, 4)}
+                          </p>
+                        </div>
+                      </div>
+                    </Link>
+                  )
                 )}
-              </CarouselContent>
 
-              <CarouselPrevious className="rounded-3xl border-none rounded-tl-none rounded-bl-none bg-black/50 hover:bg-red-600 absolute left-0 h-full md:w-20  w-12 items-center " />
-
-              <CarouselNext className="rounded-3xl border-none rounded-tr-none rounded-br-none bg-black/50 hover:bg-red-600 absolute right-0 h-full md:w-20  w-12 items-center " />
-            </Carousel>
+              {hasNextPage && items && (
+                <div className=" " ref={ref}>
+                  <Skeleton className="h-[220px] w-[110px] md:h-[380px] md:w-[200px] flex flex-col-reverse gap-2 items-center  p-2  bg-neutral-800 relative">
+                    <Skeleton className="  w-10 h-4  bg-neutral-600" />
+                    <Skeleton className="   w-full h-4   bg-neutral-600" />
+                    <Skeleton className=" w-8 h-8   md:w-14 md:h-14 rounded-full -translate-x-[80%] bg-neutral-600" />
+                    <img
+                      src="/assets/icons/Spinner-2.gif"
+                      alt=""
+                      className="absolute w-10 h-10 top-1/3"
+                    />
+                  </Skeleton>
+                </div>
+              )}
+            </Slider>
           </>
         )}
       </main>
--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,9 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import "./global.css";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
 import QueryProvider from "./lib/react-query/Quert-Provider";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
